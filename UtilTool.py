21b892808d9ec9d3e40ba66d5019955bd2c8177f
wcss <wcss2010@qq.com>
2020-05-23 17:49:26 +0800

    ...

---
+++ UtilTool.py
@@ -0,0 +1,56 @@
+#-*- coding:utf-8 -*-
+import sys
+import os
+import pathlib
+import json
+
+class UtilTool(object):
+    '''
+      读入所有文本
+    '''
+    def readAllText(fPath):
+        result = ""
+        if pathlib.Path(fPath).exists():
+            try:
+                f = open(fPath,mode='r',encoding='utf-8')
+                result = f.read()
+                f.close()
+            except IOError as e:
+                print(e)            
+        return result
+
+    '''
+      读入所有字节
+    '''
+    def readAllByte(fPath):
+        result = b''
+        if pathlib.Path(fPath).exists():
+            try:
+                f = open(fPath,mode='rb')
+                result = f.read()
+                f.close()
+            except IOError as e:
+                print(e)            
+        return result
+
+    '''
+      写入所有文本
+    '''
+    def writeAllText(fPath,strContent):
+        try:
+            f = open(fPath,mode='w',encoding='utf-8')
+            f.write(strContent)
+            f.close()
+        except IOError as e:
+            print(e)
+
+    '''
+      写入所有字节
+    '''
+    def writeAllByte(fPath,byteContent):
+        try:
+            f = open(fPath,mode='wb')
+            f.write(byteContent)
+            f.close()
+        except IOError as e:
+            print(e)
