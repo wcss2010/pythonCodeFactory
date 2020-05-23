ad754adc57125db951685f58c321b586959d8782
wcss <wcss2010@qq.com>
2020-05-23 15:58:29 +0800

    ...

--- Event_mainWindow.py
+++ Event_mainWindow.py
@@ -6,18 +6,63 @@ from PyQt5.QtNetwork import *
 import os
 import sys
 import pathlib
+import json
 from ui.Ui_mainWindow import Ui_MainWindow
 
 class Event_mainWindow(object):
   def setMainEvent(self,mainWindowThread,mainUIDefine):
-    #保存引用
-    self.mainWindow = mainWindowThread #主窗体线程
-    self.mainUI = mainUIDefine  #窗体定义类
+    #保存引用，主窗体线程
+    self.mainWindow = mainWindowThread
+    #保存引用，窗体定义类
+    self.mainUI = mainUIDefine
+    #屏蔽最大化按钮
     self.mainWindow.setFixedSize(1167, 635)
-
+    #生成脚本路径
+    self.scriptDir = os.path.join(os.getcwd(),"scripts")
+    #生成配置文件路径
+    self.configPath = os.path.join(os.getcwd(),'config.json')
+    self.configObj = {}
+    #载入配置
+    self.loadConfig()
+    #加载甜酸器类型
+    self.loadAdapterList()
     #设置事件
+    self.setEvents()
+
+  def loadConfig(self):
+    if pathlib.Path(self.configPath).exists():
+      try:
+        f = open(self.configPath,mode='r',encoding='utf-8')
+        self.configObj = json.loads(f.read())
+        f.close()
+      except Exception as e:
+        print(e)
+    else:
+      try:
+        self.configObj['adapters'] = {'xxxxDBCode':{'title':'xxx','command':'python3 xxx.py input.json output.json'}}
+        f = open(self.configPath,mode='w+',encoding='utf-8')
+        f.write(json.dumps(self.configObj))
+        f.close()
+      except Exception as e:
+        print(e)
+  
+  def loadAdapterList(self):
+    #内置
+    self.mainUI.cbxDBAdapters.addItem('Sqlite数据库(内置)')
+    #外置    
+    if self.configObj.get('adapters') == None:
+       pass
+    else:
+       self.adapterList = self.configObj.get('adapters')
+       print(self.adapterList)
+       for k,v in self.adapterList.items():
+         self.mainUI.cbxDBAdapters.addItem(v['title'] + '数据库(外置)',userData=v)
+  
+  def setEvents(self):
+    pass
     #self.mainUI.btnEncode.clicked.connect(self.btnEncodeClicked)
     #self.mainUI.btnDecode.clicked.connect(self.btnDecodeClicked)
+
   def btnEncodeClicked(self,e):
     pass
     #if self.mainUI.txtCode.toPlainText() == None:
@@ -29,8 +74,7 @@ class Event_mainWindow(object):
     #        img.save(imgFile)
     #        self.mainUI.lblImage.setPixmap(QPixmap(imgFile))
     #    except Exception as ex:
-    #        self.mainUI.statusbar.showMessage('生成错误...Ex:' + ex)
-    
+    #        self.mainUI.statusbar.showMessage('生成错误...Ex:' + ex)    
   def btnDecodeClicked(self,e):
     pass
     #imgFile,imgFormat = QFileDialog.getOpenFileName(None,'选择二维码图片','~/','图片文件(*.png *.jpg *.jpeg)')
