12ace8f03e34132c01ae26a10ec716b6adcd93f7
wcss <wcss2010@qq.com>
2020-05-23 17:14:01 +0800

    。。。

---
+++ JsCompile.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+import sys
+import os
+import pathlib
+import json
+import js2py
+
+class JsCompile(object):
+    def __init__(self,jsCode,dbUrl,tables):
+        self.scriptCode = jsCode
+        self.dbUrl = dbUrl
+        self.dbTables = tables
+    def execute(self):
+        script = js2py.eval_js(self.scriptCode)
+        return script(self.dbUrl,self.dbTables)
