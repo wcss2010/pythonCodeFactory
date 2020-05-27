8b467f9a269dc16707e79328cc7941970159ebf9
wcss <wcss2010@qq.com>
2020-05-27 22:16:06 +0800

    ...

--- scripts/normal.js
+++ scripts/normal.js
@@ -32,16 +32,57 @@
 //
 // 返回结果为一个Json字符串，格式如下：
 //  {
-//     "代码段名称1(在实体和DAO脚本中为文件名去掉.cs)":"代码内容"
-//     "代码段名称2(在实体和DAO脚本中为文件名去掉.cs)":"代码内容"
-//     "代码段名称3(在实体和DAO脚本中为文件名去掉.cs)":"代码内容"
+//     "代码段名称1(在实体和DAO脚本中为文件名去掉.cs)":"代码内容",
+//     "代码段名称2(在实体和DAO脚本中为文件名去掉.cs)":"代码内容",
+//     "代码段名称3(在实体和DAO脚本中为文件名去掉.cs)":"代码内容",
 //     "代码段名称4(在实体和DAO脚本中为文件名去掉.cs)":"代码内容"
+//  }
 //
 function script(url,table,config)
 {
    //取表名
-   var tableNames = table["tableName"];
+   var tableName = table["tableName"];
    //取名称空间
-   var namespaces = config["classNamespace"]
-   return "{\"" + tableName + "\"Entity:\"Entity内容_常用\",\"" + tableName + "DAO\":\"DAO内容_常用\"}";
+   var namespace = config["classNamespace"]
+   //清空Json串缓冲
+   var jsonBuffer="";
+   
+   //字符缓冲1
+   var tempStr1 = "";
+   //字符缓冲2
+   var tempStr2 = "";
+   
+   //例子查询语句
+   tempStr1 = appendLineTo(tempStr1,"select * from " + tableName + " where xx = '值'");
+   
+   //例子更新语句
+   tempStr2 = appendLineTo(tempStr2,"update " + tableName + " set xx = '值' where xx = '值'");
+   
+   //加入代码段1
+   appendToJson(jsonBuffer,"xx查询语句代码段",tempStr1);
+   //加入代码段2
+   appendToJson(jsonBuffer,"xx更新语句代码段",tempStr2);
+   //返回Json字符串
+   return "{" + jsonBuffer + "}";
+}
+
+//添加内容到Json串缓冲
+function appendToJson(buffer,codeName,codeContent)
+{
+	if (buffer==""){
+	   buffer =  buffer + "\"" + codeName + "\":\"" + codeContent + "\"";
+	}else{
+	   buffer =  buffer +  ",\"" + codeName + "\":\"" + codeContent + "\"";
+	}
+	return buffer;
+}
+//用于模仿C#下的StringBuilder.Append
+function appendTo(buffer,content)
+{
+	return buffer + content;	
+}
+//用于模仿C#下的StringBuilder.AppendLine
+function appendLineTo(buffer,content)
+{
+	return buffer + content + "\n";
 }
