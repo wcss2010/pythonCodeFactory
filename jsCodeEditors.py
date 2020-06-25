d6ff5f97da7522816d02cfcd3f39bd11012d64e6
wcss2010 <wcss2010@qq.com>
2020-06-25 20:33:23 +0800

    JS代码编辑器控件

---
+++ jsCodeEditors.py
@@ -0,0 +1,196 @@
+#-*- coding:utf-8 -*-
+import sys
+import os
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import numpy as np
+
+__version__ = "1.1.0"
+
+class LineNumberArea(QWidget):
+    def __init__(self, editor):
+        super().__init__(editor)
+        self.myeditor = editor
+    def sizeHint(self):
+        return Qsize(self.editor.lineNumberAreaWidth(), 0)
+    def paintEvent(self, event):
+        self.myeditor.lineNumberAreaPaintEvent(event)
+
+class JavaScriptHighlighter(QSyntaxHighlighter):
+    Rules = []
+    Formats = {}
+
+    def __init__(self, parent=None):
+        super(JavaScriptHighlighter, self).__init__(parent)
+        self.initializeFormats()
+        KEYWORDS = ["abstract", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "debugger", "default", "delete", "do", "double", "else", "enum", "export", "extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "import", "in", "instanceof", "int", "interface", "long", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "volatile", "while", "with", "array", "new", "append", "appendLine", "toString", "beginJson", "endJson", "appendToJson"]
+        JavaScriptHighlighter.Rules.append((QRegExp(
+                "|".join([r"\b%s\b" % keyword for keyword in KEYWORDS])),
+                "keyword"))
+        JavaScriptHighlighter.Rules.append((QRegExp(
+                r"\b[+-]?[0-9]+[lL]?\b"
+                r"|\b[+-]?0[xX][0-9A-Fa-f]+[lL]?\b"
+                r"|\b[+-]?[0-9]+(?:\.[0-9]+)?(?:[eE][+-]?[0-9]+)?\b"),
+                "number"))
+        JavaScriptHighlighter.Rules.append((QRegExp(r"\b@\w+\b"),
+                "decorator"))
+        stringRe = QRegExp(r"""(?:'[^']*'|"[^"]*")""")
+        stringRe.setMinimal(True)
+        JavaScriptHighlighter.Rules.append((stringRe, "string"))
+        self.stringRe = QRegExp(r"""(:?"["]".*"["]"|'''.*''')""")
+        self.stringRe.setMinimal(True)
+        JavaScriptHighlighter.Rules.append((self.stringRe, "string"))
+        self.tripleSingleRe = QRegExp(r"""'''(?!")""")
+        self.tripleDoubleRe = QRegExp(r'''"""(?!')''')
+
+    @staticmethod
+    def initializeFormats():
+        baseFormat = QTextCharFormat()
+        baseFormat.setFontFamily("courier")
+        baseFormat.setFontPointSize(12)
+        for name, color in (("normal", Qt.black),
+                ("keyword", Qt.darkRed),
+                ("decorator", Qt.darkBlue), ("comment", Qt.darkGreen),
+                ("string", Qt.darkGray), ("number", Qt.darkMagenta),
+                ("error", Qt.darkCyan)):
+            format = QTextCharFormat(baseFormat)
+            format.setForeground(QColor(color))
+            if name in ("keyword", "decorator"):
+                format.setFontWeight(QFont.Bold)
+            if name == "comment":
+                format.setFontItalic(True)
+            JavaScriptHighlighter.Formats[name] = format
+    def highlightBlock(self, text):
+        NORMAL, TRIPLESINGLE, TRIPLEDOUBLE, ERROR = range(4)
+        textLength = len(text)
+        prevState = self.previousBlockState()
+        self.setFormat(0, textLength,JavaScriptHighlighter.Formats["normal"])
+        if text.startswith("Traceback") or text.startswith("Error: "):
+            self.setCurrentBlockState(ERROR)
+            self.setFormat(0, textLength,JavaScriptHighlighter.Formats["error"])
+            return
+        if (prevState == ERROR and
+            not (text.startswith(sys.ps1) or text.startswith("/"))):
+            self.setCurrentBlockState(ERROR)
+            self.setFormat(0, textLength,JavaScriptHighlighter.Formats["error"])
+            return
+
+        for regex, format in JavaScriptHighlighter.Rules:
+            i = regex.indexIn(text)
+            while i >= 0:
+                length = regex.matchedLength()
+                self.setFormat(i, length,
+                               JavaScriptHighlighter.Formats[format])
+                i = regex.indexIn(text, i + length)
+
+        # Slow but good quality highlighting for comments. For more
+        # speed, comment this out and add the following to __init__:
+        # JavaScriptHighlighter.Rules.append((QRegExp(r"#.*"), "comment"))
+        if not text:
+            pass
+        elif text[0] == "/":
+            self.setFormat(0, len(text),JavaScriptHighlighter.Formats["comment"])
+        else:
+            stack = []
+            for i, c in enumerate(text):
+                if c in ('"', "'"):
+                    if stack and stack[-1] == c:
+                        stack.pop()
+                    else:
+                        stack.append(c)
+                elif c == "/" and len(stack) == 0:
+                    self.setFormat(i, len(text), JavaScriptHighlighter.Formats["comment"])
+                    break
+
+        self.setCurrentBlockState(NORMAL)
+        if self.stringRe.indexIn(text) != -1:
+            return
+        # This is fooled by triple quotes inside single quoted strings
+        for i, state in ((self.tripleSingleRe.indexIn(text),
+                          TRIPLESINGLE),
+                         (self.tripleDoubleRe.indexIn(text),
+                          TRIPLEDOUBLE)):
+            if self.previousBlockState() == state:
+                if i == -1:
+                    i = text.length()
+                    self.setCurrentBlockState(state)
+                self.setFormat(0, i + 3,
+                               JavaScriptHighlighter.Formats["string"])
+            elif i > -1:
+                self.setCurrentBlockState(state)
+                self.setFormat(i, text.length(),
+                               JavaScriptHighlighter.Formats["string"])
+
+    def rehighlight(self):
+        QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
+        QSyntaxHighlighter.rehighlight(self)
+        QApplication.restoreOverrideCursor()
+
+class JSCodeEditor(QPlainTextEdit):
+    def __init__(self,parent:QWidget=None):
+        super().__init__(parent)
+        self.highlighter = JavaScriptHighlighter(self.document())
+        self.lineNumberArea = LineNumberArea(self)
+        self.blockCountChanged.connect(self.updateLineNumberAreaWidth)
+        self.updateRequest.connect(self.updateLineNumberArea)
+        self.cursorPositionChanged.connect(self.highlightCurrentLine)
+        self.updateLineNumberAreaWidth(0)
+
+    def lineNumberAreaWidth(self):
+        digits = 1
+        count = max(1, self.blockCount())
+        while count >= 10:
+            count /= 10
+            digits += 1
+        space = 3 + self.fontMetrics().width('9') * digits
+        return space
+
+    def updateLineNumberAreaWidth(self, _):
+        self.setViewportMargins(self.lineNumberAreaWidth(), 0, 0, 0)
+
+    def updateLineNumberArea(self, rect, dy):
+        if dy:
+            self.lineNumberArea.scroll(0, dy)
+        else:
+            self.lineNumberArea.update(0, rect.y(), self.lineNumberArea.width(),rect.height())
+
+        if rect.contains(self.viewport().rect()):
+            self.updateLineNumberAreaWidth(0)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        cr = self.contentsRect()
+        self.lineNumberArea.setGeometry(QRect(cr.left(), cr.top(),self.lineNumberAreaWidth(), cr.height()))
+
+    def lineNumberAreaPaintEvent(self, event):
+        mypainter = QPainter(self.lineNumberArea)
+        mypainter.fillRect(event.rect(), Qt.lightGray)
+        block = self.firstVisibleBlock()
+        blockNumber = block.blockNumber()
+        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top()
+        bottom = top + self.blockBoundingRect(block).height()
+        # Just to make sure I use the right font
+        height = self.fontMetrics().height()
+        while block.isValid() and (top <= event.rect().bottom()):
+            if block.isVisible() and (bottom >= event.rect().top()):
+                number = str(blockNumber + 1)
+                mypainter.setPen(Qt.black)
+                mypainter.drawText(0, top, self.lineNumberArea.width(), height,Qt.AlignRight, number)
+
+            block = block.next()
+            top = bottom
+            bottom = top + self.blockBoundingRect(block).height()
+            blockNumber += 1
+
+    def highlightCurrentLine(self):
+        extraSelections = []
+        if not self.isReadOnly():
+            selection = QTextEdit.ExtraSelection()
+            lineColor = QColor(Qt.yellow).lighter(160)
+            selection.format.setBackground(lineColor)
+            selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+            extraSelections.append(selection)
+        self.setExtraSelections(extraSelections)
