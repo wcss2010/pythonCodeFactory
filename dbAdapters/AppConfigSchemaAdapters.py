a6624fd5f3a27d317762f5c55567a63930c3ef49
wcss2010 <wcss2010@qq.com>
2020-07-03 23:20:09 +0800

    ...

--- dbAdapters/AppConfigSchemaAdapters.py
+++ dbAdapters/AppConfigSchemaAdapters.py
@@ -10,16 +10,17 @@ import subprocess
 import io
 
 class AppConfigSchemaAdapter(BaseAdapter):
-    def getTables(self,dbUrl):       
+    def getTables(self,dbUrl):
+       #初始化变量
        result = 'error'
        nameStr = self.dbAdapterInfo['title']
        codeStr = self.dbAdapterInfo['code']
        cmdStr = self.dbAdapterInfo['command']
        responseCoding = self.dbAdapterInfo['responseCoding']
-       #检查输入配置,输出配置,插件工作目录
-       inputDir = os.path.join(os.getcwd(),'inputTempDir')
-       outputDir = os.path.join(os.getcwd(),'outputTempDir')
-       pluginDir = os.path.join(os.getcwd(),'dbAdapters')
+
+       #检查输入配置,输出配置
+       inputDir = os.path.join(cfenv.dataDir,'inputTempDir')
+       outputDir = os.path.join(cfenv.dataDir,'outputTempDir')
        try:
            os.mkdir(inputDir)
        except IOError as e:
@@ -28,26 +29,22 @@ class AppConfigSchemaAdapter(BaseAdapter):
            os.mkdir(outputDir)
        except IOError as e:
           print(e)
+        
        #生成输入和输出配置路径
        inputFile = os.path.join(inputDir,'input_' + str(time.time()) + ".json")
        outputFile = os.path.join(outputDir,'output_' + str(time.time()) + ".json")
        #写输入配置
        inputConfig = AdapterInputConfig(dbUrl,codeStr)
        iotool.writeAllText(inputFile,json.dumps(inputConfig.toDict(),indent=4))
-       #运行外部程序获得数据库结构
-       nowCmd = cmdStr.format(local=pluginDir, input=inputFile, output=outputFile)
-       print('db command:' + nowCmd)
-       proc = subprocess.Popen(nowCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
-       proc.wait()
-       stream_stdout = io.TextIOWrapper(proc.stdout, encoding=responseCoding)
-       stream_stderr = io.TextIOWrapper(proc.stderr, encoding=responseCoding)      
-       str_stdout = str(stream_stdout.read())
-       str_stderr = str(stream_stderr.read())
-       print("stdout: " + str_stdout)
-       print("stderr: " + str_stderr)
-       result = (str_stdout + str_stderr).strip()
-       print('db command result:' + result)
+
+       #生成命令字符串
+       dbCmd = cmdStr.format(local=cfenv.dbPluginDir, input=inputFile, output=outputFile)
+       
+       #运行数据适配器程序等待结果返回
+       allStr,normalStr,errorStr = iotool.start(dbCmd,responseCoding)
+
+       #返回数据集
        runResult = {}
        if pathlib.Path(outputFile).exists():
            runResult = json.loads(iotool.readAllText(outputFile))
-       return result,runResult
+       return allStr,runResult
