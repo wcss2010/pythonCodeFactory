8d1f3d7d86e32b2f836b21a202bc7807333c9915
wcss2010 <wcss2010@qq.com>
2020-07-04 01:07:41 +0800

    ...

--- Event_mainWindows.py
+++ Event_mainWindows.py
@@ -25,10 +25,9 @@ class Event_mainWindow(object):
     self.mainWindow.setFixedSize(1167, 635)
     #隐藏提示框
     self.hideWaitBox()
-    #生成脚本路径
-    self.scriptDir = os.path.join(os.getcwd(),"scripts")
-    #生成配置文件路径
-    self.configPath = os.path.join(os.getcwd(),'config.json')
+    #初始化环境变量
+    self.initEnv()
+    #初始化配置对象
     self.configObj = {}
     #载入配置
     self.loadConfig()
@@ -39,11 +38,17 @@ class Event_mainWindow(object):
     #初始化
     self.initData()
 
+  def initEnv(self):
+    cfenv.configFilePath = os.path.join(cfenv.rootDir,'config.json')
+    cfenv.templeteScriptFile = os.path.join(cfenv.rootDir,'templete.js')
+    cfenv.normalScriptFile = os.path.join(cfenv.scriptDir,"normal.js")
+    cfenv.entityAndDAOScriptFile = os.path.join(cfenv.scriptDir,"entityanddao.js")
+
   def loadConfig(self):
-    if pathlib.Path(self.configPath).exists():
+    if pathlib.Path(cfenv.configFilePath).exists():
       #读入数据
       try:
-        jsonStr = iotool.readAllText(self.configPath)
+        jsonStr = iotool.readAllText(cfenv.configFilePath)
         self.configObj = json.loads(jsonStr)
       except Exception as exx:
         self.initConfig()        
@@ -52,14 +57,14 @@ class Event_mainWindow(object):
   
   def initConfig(self):
     #初始化的例子
-    self.configObj['adapters'] = {'xxxxCode':{'title':'xxxDB','code':'xxxxCode','command':'python3 {local}/xxx.py {input} {output}','responseCoding':'utf8'}}
+    self.configObj['dbPlugins'] = {'xxxxCode':{'title':'xxxDB','code':'xxxxCode','command':'python3 {local}/xxx.py {input} {output}','responseCoding':'utf8'}}
     self.configObj['codeFileExtName'] = '.cs'
     self.configObj['classNameBefore'] = 't'
     self.configObj['classNameAfter'] = 'Object'
     self.configObj['classNamespace'] = 'com.example'
     self.configObj['dialogRootDir'] = '~/'
     #写入数据
-    iotool.writeAllText(self.configPath,json.dumps(self.configObj,indent=4))
+    iotool.writeAllText(cfenv.configFilePath,json.dumps(self.configObj,indent=4))
   
   def loadAdapterList(self):
     self.mainUI.cbxDBAdapters.clear()
@@ -68,15 +73,15 @@ class Event_mainWindow(object):
     sqliteAdapter.initAdapter(None)
     self.mainUI.cbxDBAdapters.addItem('Sqlite数据库(内置)',userData=sqliteAdapter)
     #外置
-    if self.configObj.get('adapters') == None:
+    if self.configObj.get('dbPlugins') == None:
        pass
     else:
-       self.adapterList = self.configObj.get('adapters')
+       self.adapterList = self.configObj.get('dbPlugins')
        print(self.adapterList)
        for k,v in self.adapterList.items():
          appAdapter = AppConfigSchemaAdapter()
          appAdapter.initAdapter(v)
-         self.mainUI.cbxDBAdapters.addItem(v['title'] + '数据库(外置)',userData=appAdapter)
+         self.mainUI.cbxDBAdapters.addItem(v['title'] + '(插件)',userData=appAdapter)
   
   def setEvents(self):    
     self.mainUI.btnOpenDB.clicked.connect(self.btnOpenDBClicked)
@@ -96,42 +101,36 @@ class Event_mainWindow(object):
   def initData(self):
     self.nodeModels = QStandardItemModel()
     #加载配置文件
-    if (pathlib.Path(self.configPath).exists()):
-        self.mainUI.txtConfig.setText(iotool.readAllText(self.configPath))
-    #检查是否脚本没有创建
-    scriptDir = os.path.join(os.getcwd(),'scripts')
-    if (pathlib.Path(scriptDir).exists()):
-        pass
-    else:
-        os.makedirs(scriptDir)
-    templeteFile = os.path.join(os.getcwd(),'templete.js')
-    #初始化常用代码脚本
-    self.normalScriptFile = os.path.join(scriptDir,"normal.js")
-    if (pathlib.Path(self.normalScriptFile).exists()):
+    if (pathlib.Path(cfenv.configFilePath).exists()):
+        self.mainUI.txtConfig.setText(iotool.readAllText(cfenv.configFilePath))
+        
+    #初始化常用代码脚本    
+    if (pathlib.Path(cfenv.normalScriptFile).exists()):
       pass
     else:
-      if (pathlib.Path(templeteFile).exists()):
+      if (pathlib.Path(cfenv.templeteScriptFile).exists()):
           try:
-            shutil.copyfile(templeteFile,self.normalScriptFile)
+            shutil.copyfile(cfenv.templeteScriptFile,cfenv.normalScriptFile)
           except Exception as e:
             print(e)
       else:
-        iotool.writeAllText(self.normalScriptFile)
+        iotool.writeAllText(cfenv.normalScriptFile,'')
+    
     #初始化实体和DAO代码脚本
-    self.entityAndDAOScriptFile = os.path.join(scriptDir,"entityanddao.js")
-    if (pathlib.Path(self.entityAndDAOScriptFile).exists()):
+    if (pathlib.Path(cfenv.entityAndDAOScriptFile).exists()):
       pass
     else:
-      if (pathlib.Path(templeteFile).exists()):
+      if (pathlib.Path(cfenv.templeteScriptFile).exists()):
           try:
-            shutil.copyfile(templeteFile,self.entityAndDAOScriptFile)
+            shutil.copyfile(cfenv.templeteScriptFile,cfenv.entityAndDAOScriptFile)
           except Exception as e:
             print(e)
       else:
-        iotool.writeAllText(self.entityAndDAOScriptFile)
+        iotool.writeAllText(cfenv.entityAndDAOScriptFile,'')
+    
     #显示脚本内容
-    self.mainUI.txtNormalScript.setText(iotool.readAllText(self.normalScriptFile))
-    self.mainUI.txtEntityAndDAOScript.setText(iotool.readAllText(self.entityAndDAOScriptFile))
+    self.mainUI.txtNormalScript.setText(iotool.readAllText(cfenv.normalScriptFile))
+    self.mainUI.txtEntityAndDAOScript.setText(iotool.readAllText(cfenv.entityAndDAOScriptFile))
   
   def showWaitBox(self,title,content):
     self.mainUI.plWaitBox.setTitle('    ' + title)
@@ -190,23 +189,25 @@ class Event_mainWindow(object):
             pass
           else:
             #载入脚本代码
-            normalScript = iotool.readAllText(self.normalScriptFile)
-            entityAndDAOScript = iotool.readAllText(self.entityAndDAOScriptFile)
+            normalScript = iotool.readAllText(cfenv.normalScriptFile)
+            entityAndDAOScript = iotool.readAllText(cfenv.entityAndDAOScriptFile)
             #编译执行
             try:
               normalDisplayText = ''
               entityDisplayText = ''
               #编译并显示常用代码
               normalResult = json.loads(self.compileJS(normalScript,tableData))
-              for kk,vv in normalResult.items():
-                  normalDisplayText = normalDisplayText+'代码段('+kk+'):\n'
-                  normalDisplayText = normalDisplayText+vv+';\n'
+              for cName1,cSource1 in normalResult.items():
+                  codeText1 = stringbuffer().fromB64String(cSource1).toString()
+                  normalDisplayText = normalDisplayText+'代码段('+cName1+'):\n'
+                  normalDisplayText = normalDisplayText+codeText1+'\n'
               self.mainUI.txtNormalText.setPlainText(normalDisplayText)
               #编译并显示实体和DAO代码
               entityResult = json.loads(self.compileJS(entityAndDAOScript,tableData))
-              for kkk,vvv in entityResult.items():
-                  entityDisplayText = entityDisplayText+'代码段('+kkk+'):\n'
-                  entityDisplayText = entityDisplayText+vvv+';\n'
+              for cName2,cSource2 in entityResult.items():
+                  codeText2 = stringbuffer().fromB64String(cSource2).toString()
+                  entityDisplayText = entityDisplayText+'代码段('+cName2+'):\n'
+                  entityDisplayText = entityDisplayText+codeText2+'\n'
               self.mainUI.txtEntityAndDAOText.setPlainText(entityDisplayText)
             except Exception as ex:
               print(ex)
@@ -222,12 +223,11 @@ class Event_mainWindow(object):
         destPath=QFileDialog.getExistingDirectory(None,"请选择保存位置！",self.configObj["dialogRootDir"])
         #生成目标路径      
         destCodePath = os.path.join(destPath,'classes')
-        destAttachPath = os.path.join(destPath,'attachs')
-        sourceAttachPath = os.path.join(os.getcwd(),'codeAttachs')
+        destAttachPath = os.path.join(destPath,'attachs')        
         #复制附件目录到
-        if pathlib.Path(sourceAttachPath).exists():
+        if pathlib.Path(cfenv.attachDir).exists():
           try:
-            shutil.copytree(sourceAttachPath,destAttachPath)
+            shutil.copytree(cfenv.attachDir,destAttachPath)
           except Exception as ex:
             print(ex)
         #创建代码目录
@@ -241,7 +241,7 @@ class Event_mainWindow(object):
           classAfterName = self.configObj["classNameAfter"]
           codeFileExtName = self.configObj["codeFileExtName"]
           #读入实体和DAO脚本
-          entityScript = iotool.readAllText(self.entityAndDAOScriptFile)
+          entityScript = iotool.readAllText(cfenv.entityAndDAOScriptFile)
           for rowIndex in range(0,self.nodeModels.rowCount()):
             itemRow = self.nodeModels.item(rowIndex)
             if itemRow==None or itemRow.data()==None:
@@ -252,9 +252,10 @@ class Event_mainWindow(object):
                 pass
               else:              
                 entityResult = json.loads(self.compileJS(entityScript,tableData))
-                for kkk,vvv in entityResult.items():
-                  destFile = os.path.join(destCodePath,kkk + codeFileExtName)
-                  iotool.writeAllText(destFile,vvv)
+                for cName,cSource in entityResult.items():
+                  cText = stringbuffer().fromB64String(cSource).toString()
+                  destFile = os.path.join(destCodePath,cName + codeFileExtName)
+                  iotool.writeAllText(destFile,cText)
           QMessageBox.information(None,"提示","代码生成完成！")
       except Exception as vv:
         print(vv)
@@ -265,24 +266,24 @@ class Event_mainWindow(object):
 
   def btnSaveNormalScriptClicked(self,e):
     try:
-      iotool.writeAllText(self.normalScriptFile,self.mainUI.txtNormalScript.toPlainText())
+      iotool.writeAllText(cfenv.normalScriptFile,self.mainUI.txtNormalScript.toPlainText())
       QMessageBox.information(None,"提示","保存完成!")
     except IOError as e:
       QMessageBox.information(None,"错误","保存失败!输出：" + e)
 
   def btnSaveEntityAndDAOScriptClicked(self,e):
     try:
-      iotool.writeAllText(self.entityAndDAOScriptFile,self.mainUI.txtEntityAndDAOScript.toPlainText())
+      iotool.writeAllText(cfenv.entityAndDAOScriptFile,self.mainUI.txtEntityAndDAOScript.toPlainText())
       QMessageBox.information(None,"提示","保存完成!")
     except IOError as e:
       QMessageBox.information(None,"错误","保存失败!输出：" + e)
 
   def btnSaveConfigClicked(self,e):
     try:
-      iotool.writeAllText(self.configPath,self.mainUI.txtConfig.toPlainText())
+      iotool.writeAllText(cfenv.configFilePath,self.mainUI.txtConfig.toPlainText())
       self.loadConfig()
       self.loadAdapterList()
-      self.mainUI.txtConfig.setText(iotool.readAllText(self.configPath))
+      self.mainUI.txtConfig.setText(iotool.readAllText(cfenv.configFilePath))
       QMessageBox.information(None,"提示","保存完成!")
     except Exception as e:
       QMessageBox.information(None,"错误","保存失败!输出：" + e)
@@ -307,13 +308,13 @@ class Event_mainWindow(object):
 
   def btnDownloadJSClicked(self,e):    
     inputFile,inputFormat = QFileDialog.getSaveFileName(None,"选择脚本模板保存位置",self.configObj["dialogRootDir"],"JS脚本[JS](*.js)")
-    iotool.writeAllText(inputFile,iotool.readAllText(os.path.join(os.getcwd(),"templete.js")))
+    iotool.writeAllText(inputFile,iotool.readAllText(cfenv.templeteScriptFile))
     QMessageBox.information(None,"提示","保存完成！")
 
   def btnOpenAttachDirClicked(self,e):
     try:
       if sys.platform == 'win32':
-        os.startfile(os.path.join(os.getcwd(),"codeAttachs"))
+        os.startfile(cfenv.attachDir)
       else:
         QMessageBox.information(None,"提示","对不起，这个功能只能在windows下用！")
     except Exception as ex:
@@ -322,7 +323,7 @@ class Event_mainWindow(object):
   def btnOpenPluginDirClicked(self,e):
     try:
       if sys.platform == 'win32':
-        os.startfile(os.path.join(os.getcwd(),"dbAdapters"))
+        os.startfile(cfenv.dbPluginDir)
       else:
         QMessageBox.information(None,"提示","对不起，这个功能只能在windows下用！")
     except Exception as ex:
@@ -331,7 +332,7 @@ class Event_mainWindow(object):
   def btnOpenScriptDirClicked(self,e):
     try:
       if sys.platform == 'win32':
-        os.startfile(os.path.join(os.getcwd(),"scripts"))
+        os.startfile(cfenv.scriptDir)
       else:
         QMessageBox.information(None,"提示","对不起，这个功能只能在windows下用！")
     except Exception as ex:
