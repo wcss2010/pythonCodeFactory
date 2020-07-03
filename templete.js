235c28afd9379f7b627ab6ebcbaae6a1902d6b7e
wcss2010 <wcss2010@qq.com>
2020-07-04 04:46:02 +0800

    ...

--- templete.js
+++ templete.js
@@ -14,119 +14,85 @@
 // }
 //config=格式为:
 //{
-//   "adapters": {
-//      "OracleDBCode": {
-//          "title": "Oracle",
-//          "command": "java -version",
-//          "inputFile": "input.json",
-//          "outputFile": "output.json"
-//          "responseCoding": "gbk"
-//      }
-//  },
-//  "codeFileExtName": ".cs",
-//  "classNameBefore": "t",
-//  "classNameAfter": "Object",
-//  "classNamespace": "com.example"
-//  "dialogRootDir": "~/"
+//    "dbPlugins": {
+//        "xxxxCode": {
+//            "title": "xxxDB",
+//            "code": "xxxxCode",
+//            "command": "python3 {local}/xxx.py {input} {output}",
+//            "responseCoding": "utf8"
+//        }
+//    },
+//    "codeFileExtName": ".cs",
+//    "classNameBefore": "t",
+//    "classNameAfter": "Object",
+//    "classNamespace": "com.example",
+//    "dialogRootDir": "~/"
 //}
 //
 // 返回结果为一个Json字符串，格式如下：
 //  {
-//     "代码段名称1(在实体和DAO脚本中为文件名去掉.cs)":"代码内容",
-//     "代码段名称2(在实体和DAO脚本中为文件名去掉.cs)":"代码内容",
-//     "代码段名称3(在实体和DAO脚本中为文件名去掉.cs)":"代码内容",
-//     "代码段名称4(在实体和DAO脚本中为文件名去掉.cs)":"代码内容"
+//     "代码段名称1(在实体和DAO脚本中为文件名去掉.cs)":"代码内容(需要转换成Base64编码，以保证 \ " ' / ? 等符号不会干扰JSON解析)",
+//     "代码段名称2(在实体和DAO脚本中为文件名去掉.cs)":"代码内容(需要转换成Base64编码，以保证 \ " ' / ? 等符号不会干扰JSON解析)",
+//     "代码段名称3(在实体和DAO脚本中为文件名去掉.cs)":"代码内容(需要转换成Base64编码，以保证 \ " ' / ? 等符号不会干扰JSON解析)",
+//     "代码段名称4(在实体和DAO脚本中为文件名去掉.cs)":"代码内容(需要转换成Base64编码，以保证 \ " ' / ? 等符号不会干扰JSON解析)"
 //  }
 //
-
-//用于模仿C#下的StringBuilder.Append
-function append(buffer,content)
-{
-   buffer.push(content);
-}
-//用于模仿C#下的StringBuilder.AppendLine
-function appendLine(buffer,content)
-{
-    buffer.push(content);
-    buffer.push("\\r\\n");
-}
-//用于模仿C#下的StringBuilder.ToString()
-function toString(buffer)
-{
-    return buffer.join("");
-}
-
-//开始Json
-function beginJson(buffer)
-{
-   buffer.push("{");
-}
-//结束Json
-function endJson(buffer)
-{
-   buffer.push("}");
-}
-//添加内容到Json串缓冲
-function appendToJson(buffer,codeName,codeContent)
-{
-   //判断是否前面已经数据
-   if (buffer.length == 1)
-   {
-      buffer.push("");
-   }else
-   {
-      buffer.push(",");
-   }
-   //拼装数据
-   buffer.push("\"");
-   buffer.push(codeName);
-   buffer.push("\"");
-   buffer.push(":");
-   buffer.push("\"");
-   buffer.push(codeContent);
-   buffer.push("\"");
-}
 
 //入口函数
 function script(url,table,config)
 {
+   //引用内置的Python工具集
+   pyimport globaltool;
+   //
+   // （1）cfenv（环境变量）
+   //     rootDir(主目录变量) binDir(程序目录变量) dataDir(数据目录变量) dbPluginDir(插件目录变量) scriptDir(脚本目录变量) attachDir(附件目录变量)
+   //  (2) stringbuffer(类似于C#中的StringBuilder)
+   //     enterFlag(回车点位符变量) clear(清理函数,参数：空) append(添加字符串函数,参数：字符串) appendLine(添加字符串并在末尾加回车函数,参数：字符串) fromString(载入非Base64字符串到缓冲区,参数：字符串) toString(输出可显示字符串,参数：空) fromB64String(解码并装载Base64字符串,参数：字符串) toB64String(将缓冲区内容编码为Base64字符串,参数：空)
+   //  (3) jsoncodewriter(Json代码块生成)
+   //     append(添加代码块,参数：字符串)  remove(删除代码块,参数：字符串)  toString(输出代码块到JSON字符串,参数：空)
+   //  (4) iotool(读写工具类)
+   //     start(运行指令并等待返回结果,参数：命令字符串，返回结果字符集) readAllText(读入所有文本,参数：文件路径) readAllByte(读入所有字节,参数：文件路径) writeAllText(写入所有文本,参数：文件路径,要写入的文本) writeAllByte(写入所有字节,参数：文件路径,要写入的字节集)
+   //
+   //
+
    //取表名
    var tableName = table["tableName"];
    //取名称空间
    var namespace = config["classNamespace"]
-   //Json缓冲
-   var jsonBuf= new Array();
-   //字符缓冲1
-   var tempBuf1= new Array();
-   //字符缓冲2
-   var tempBuf2= new Array();
    
-   //开始Json
-   beginJson(jsonBuf);
+   //JSON代码块生成
+   var jsonCodeMaker = globaltool.jsoncodewriter();
    
-   //拼装例子1
-   var columns = table["columns"];
-   for(var k = 0;k < columns.length;k++)
-   {
-      var colObj = columns[k];
-      var colName = colObj["name"];
-      append(tempBuf1,colName + ",");
-   }
+   //代码块例子1
+   var sb1 = globaltool.stringbuffer();
+   sb1.append('update ').append(tableName).append(' set xxx = \"\" where xx = 1').appendLine('');
+   sb1.append('update ').append(tableName).append(' set xxx = \"\" where xx = 1').appendLine('');
+   sb1.append('update ').append(tableName).append(' set xxx = \"\" where xx = 1').appendLine('');
+   sb1.append('update ').append(tableName).append(' set xxx = \"\" where xx = 1').appendLine('');
+   sb1.append('例子1').append(globaltool.stringbuffer.enterFlag);
+   sb1.append('例子2').append(globaltool.stringbuffer.enterFlag);
+   sb1.appendLine('例子3');
+   sb1.appendLine('例子4');
 
-   //拼装例子2
-   appendLine(tempBuf2,"update " + tableName + " set xx = '值' where xx = '值'");
-   appendLine(tempBuf2,"update " + tableName + " set xx = '值' where xx = '值'");
-   appendLine(tempBuf2,"update " + tableName + " set xx = '值' where xx = '值'");
-   appendLine(tempBuf2,"update " + tableName + " set xx = '值' where xx = '值'");
+   //代码块例子2
+   var sb2 = globaltool.stringbuffer();
+   sb2.appendLine(globaltool.cfenv.dataDir);
+   sb2.appendLine(globaltool.cfenv.dbPluginDir);
+   sb2.appendLine(globaltool.cfenv.scriptDir);
+   sb2.appendLine(globaltool.cfenv.attachDir);
 
-   //加入代码段1
-   appendToJson(jsonBuf,"xx查询语句代码段",toString(tempBuf1));
-   //加入代码段2
-   appendToJson(jsonBuf,"xx更新语句代码段",toString(tempBuf2));
+   //代码块例子3
+   var sb3 = globaltool.stringbuffer();
+   pyimport os;
+   sb3.appendLine(globaltool.iotool.readAllText(os.path.join(globaltool.cfenv.rootDir,'config.json')));
 
-   //结束Json
-   endJson(jsonBuf);
+   //添加代码块1
+   jsonCodeMaker.append('第一个例子',sb1.toB64String());
+   //添加代码块2
+   jsonCodeMaker.append('第二个例子',sb2.toB64String());
+   //添加代码块3
+   jsonCodeMaker.append('第三个例子',sb3.toB64String());
 
    //返回Json字符串
-   return toString(jsonBuf);
+   return jsonCodeMaker.toString();
 }
